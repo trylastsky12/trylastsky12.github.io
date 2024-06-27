20d296205027116d4c22bb8aa1f41a64209d30b1
Cloudy_Trylastsky <146930018+trylastsky@users.noreply.github.com>
2024-06-27 14:09:13 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -50,39 +50,32 @@ class SnakeGame {
       this.touchX = e.changedTouches[0].pageX;
     });
     document.addEventListener(
-      "touchmove",
-      (e) => {
-        e.preventDefault();
+  "touchmove",
+  (e) => {
+    e.preventDefault();
 
-        const swipeDistanceY = e.changedTouches[0].pageY - this.touchY;
-        const swipeDistanceX = e.changedTouches[0].pageX - this.touchX;
-        if (
-          swipeDistanceY < -this.touchTreshold &&
-          this.snake.direction !== "down"
-        ) {
-          this.snake.direction = "up";
-        }
-        if (
-          swipeDistanceY > this.touchTreshold &&
-          this.snake.direction !== "up"
-        ) {
-          this.snake.direction = "down";
-        }
-        if (
-          swipeDistanceX > this.touchTreshold &&
-          this.snake.direction !== "left"
-        ) {
-          this.snake.direction = "right";
-        }
-        if (
-          swipeDistanceX < -this.touchTreshold &&
-          this.snake.direction !== "right"
-        ) {
-          this.snake.direction = "left";
-        }
-      },
-      { passive: false }
-    );
+    const swipeDistanceY = e.changedTouches[0].pageY - this.touchY;
+    const swipeDistanceX = e.changedTouches[0].pageX - this.touchX;
+
+    const absSwipeDistanceX = Math.abs(swipeDistanceX);
+    const absSwipeDistanceY = Math.abs(swipeDistanceY);
+
+    if (absSwipeDistanceX > absSwipeDistanceY) {
+      if (swipeDistanceX > this.touchTreshold && this.snake.direction !== "left") {
+        this.snake.direction = "right";
+      } else if (swipeDistanceX < -this.touchTreshold && this.snake.direction !== "right") {
+        this.snake.direction = "left";
+      }
+    } else {
+      if (swipeDistanceY < -this.touchTreshold && this.snake.direction !== "down") {
+        this.snake.direction = "up";
+      } else if (swipeDistanceY > this.touchTreshold && this.snake.direction !== "up") {
+        this.snake.direction = "down";
+      }
+    }
+  },
+  { passive: false }
+);
     //подключаем старт игры к кнопке старт в меню
     document
       .getElementById("play-button")
